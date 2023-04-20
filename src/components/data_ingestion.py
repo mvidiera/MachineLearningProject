f7ebbb6aa9572bdadf8d15f3a0f5907b232b5ae9
mvidiera <melissavidiera5@gmail.com>
2023-04-21 01:48:31 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from src.exception import CustomException #src-> exceptoion.py for custom exception
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass 
+#extremly important 
+#decorator:dataclass is used instead of init method to initialise the values to variables within class.here i just have variables 
+# to store, so I am using dataclass. As per definition, dataclass is used ONLY to hold data values. if i had other functions then
+# I would use init (constructor)
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts', 'train.csv')
+    test_data_path: str=os.path.join('artifacts', 'test.csv')
+    raw_data_path: str=os.path.join('artifacts', 'data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()  #this has 3 values train, test, raw data path. 
+#As soon as I call dataingestion class-> control goes to DataIngestionConfig and 3 things takes place 
+
+# basic data ingestion 
+    def initiate_data_ingestion(self):
+        logging.info("Enter the data ingestion method or component")
+        try:
+            df= pd.read_csv('notebook/data/stud.csv') # basic data reading which is in csv format. to read it from mongo, I need to make changes here alone 
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok= True)
+#os.path.dirname checks if train_data-path exist, if yes then it leaves as it is exist_ok=True, if it doesn't exist then
+#create new directory called train_data_path using makdirs
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("train test split initiated")
+            #splitting data
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            # saving train and test data into train_data_path and test_data_path variable in ingestion config class
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Ingestion of the data is completed")
+
+            #returning trainin and testing data path to next step which is transformation
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__== "__main__":
+    obj= DataIngestion()
+    obj.initiate_data_ingestion()
--- src/logger.py
+++ src/logger.py
@@ -1,13 +1,11 @@
-#any execution and all info will be logged and tracked. Anything and everything will be logged
-
 import logging
 import os
 from datetime import datetime
-
+#any execution and all info will be logged and tracked. Anything and everything will be logged
 
 LOG_FILE= f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" #file is created and date time which is in string format will be logged. 
-logs_path= os.path.join(os.getcwd, "logs", LOG_FILE) #getcwd get current working directory
-os.makedirs(logs_path, exists_ok= True)
+logs_path= os.path.join(os.getcwd(), "logs", LOG_FILE) #getcwd get current working directory
+os.makedirs(logs_path, exist_ok= True)
 
 
 LOG_FILE_PATH= os.path.join(logs_path, LOG_FILE)
@@ -17,4 +15,5 @@ logging.basicConfig(
     filename= LOG_FILE_PATH, 
     format= "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level= logging.INFO,
+
 )
